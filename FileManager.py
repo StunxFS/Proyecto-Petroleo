4880fa36defcf9a6c42d92c4d7c63b44eb3b43b2
Jose Mendoza <56417208+StunxFS@users.noreply.github.com>
2024-05-20 15:36:08 -0400

    Create FileManager.py

---
+++ FileManager.py
@@ -0,0 +1,35 @@
+import os
+
+class FileManager:
+    CWD = os.getcwd()
+    
+    def __init__(self):
+        self.filenames = filter(lambda f: f.endswith(".txt"), os.listdir(CWD))
+        
+        # Contiene la cantidad de archivos diarios o por semana
+        # que se han creado
+        self.contador = len(self.filenames)
+        
+        # Contiene el archivo actual con el que estamos trabajando.
+        # Esto sirve como cache para evitar tener que reabrir el
+        # archivo varias veces.
+        self.archivo_s_actual = None
+        self.archivo_d_actual = None
+    
+    # Devuelve el objeto del archivo actual con el que estamos
+    # trabajando los datos semanales.
+    def obtener_archivo_semanal(self, mode="a"):
+        if self.archivo_s_actual:
+            return self.archivo_s_actual
+        self.archivo_s_actual = open(self.filenames[-1], mode)
+        return self.archivo_s_actual
+    
+    # Devuelve el objeto del archivo actual con el que estamos
+    # trabajando los datos diarios. Si el archivo supera las
+    # 160 líneas, se crea un nuevo archivo y se devuelve ese.
+    def obtener_archivo_diario(self, mode="a"):
+        if self.archivo_d_actual:
+            return self.archivo_d_actual
+        self.archivo_d_actual = open(self.filenames[-1], mode)
+        
+        return self.archivo_d_actual
